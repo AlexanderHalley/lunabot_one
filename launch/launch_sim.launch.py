b93b11084fdaf33d52d0e0f6cd610c1d5bf55aa2
AlexanderHalley <alexanderhalley2023@u.northwestern.edu>
2025-07-16 18:27:32 +0200

    fixed bug in launch_sim.launch.py

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,7 +2,8 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription, DeclareLaunchArgument, IncludeLaunchDescription
+from launch import LaunchDescription, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
